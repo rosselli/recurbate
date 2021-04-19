2b5531eb676898fd18ccad6806f90face89e7fa9
Flavio Rosselli <rosselli00@gmail.com>
2021-04-19 00:44:20 -0300

    FavoriteList was implemented on the Recent Recordings.

--- requirements.md
+++ requirements.md
@@ -1,9 +1,21 @@
-# Requirements
+# Requirements: Recurbate
 
-- [x] Set Favorite and Checked (based temp files) on Local Checklist.
-- [ ] Get info from Local Checklist.
-- [ ] Sync Recurbate list with Local Checklist.
-- [ ] WriteCache UnChecked and Favorites.
-- [ ] List all videos from a specific model.
-- [ ] Crop and Optimize videos.
-- [ ] Create Mosaic Preview Image.
+
+- [x] **Recent**
+    - [x] Get Recent Recordings filtered (favorites and Blacklist).
+    - [x] Recent Viewer.
+    - [x] Generate Blacklist from Checklist (filtered by Status).
+    - [x] Generate Favoritelist from Checklist (filtered by Status).
+    - [x] Download all Images.
+- [ ] **All Performers**
+    - [x] Start the Checklist from zero.
+    - [x] Get all Categories lists manually from site (Male, Trans, Couples and Subscriptions) listed by Console Functions.
+    - [x] Get all Custom lists to manage manually (favorites and Rejected).
+    - [x] Set Checklist Status (based on Categories lists).
+    - [ ] Update weekly All Performers Checklist from site (listed by Console Functions).
+- [ ] **Specific Performer**
+    - [x] Generate the checklist of an Specific Performer. 
+    - [ ] Update the checklist of an Specific Performer.
+    - [ ] Generate Dowload list (based on unchecked items from a performer's list)
+    - [ ] Get Specific Download Link (Auth)
+- [ ] Calculate Total Pages
--- src/get-recent-recordings.js
+++ src/get-recent-recordings.js
@@ -2,10 +2,12 @@ const file = require('./helpers/files');
 const urls = require('./helpers/urls');
 const scraper = require('./helpers/scraper')
 const blacklist = require('./helpers/get-blacklist')();
+const favoritelist = require('./helpers/get-favoritelist')();
 const validateParameters = require('./helpers/cli-validations');
 
 const run = (pages) => {
 	const recent = [];
+	const favorited = [];
 	const blacklisted = [];
 	const duplicated = [];
 	Array(pages).fill().map(async (_, index) => {
@@ -13,14 +15,18 @@ const run = (pages) => {
 		const data = await scraper(urls.base(index + 1));
 		data.map(item => {
 			const inBlacklist = blacklist.includes(item.performer);
+			const inFavoritelist = favoritelist.includes(item.performer);
 			const duplicate = recent.some(inRecent => inRecent.performer === item.performer);
 			duplicate && duplicated.push(item.performer) && console.log('duplicate', item.performer);
 			inBlacklist && blacklisted.push(item.performer) && console.log('blacklisted', item.performer);
+			inFavoritelist && favorited.push(item.performer) && console.log('favorited', item.performer);
 			(!duplicate && !inBlacklist) && recent.push(item)
 		});
 		console.log({
 			recent: recent.length,
+			favoritelist: favoritelist.length,
 			blacklist: blacklist.length,
+			favorited: favorited.length,
 			blacklisted: blacklisted.length,
 			duplicated: duplicated.length,
 		});
