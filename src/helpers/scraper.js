0320051e89307e85ad5b479da9780272f59e2adc
Flavio Rosselli <rosselli00@gmail.com>
2021-05-01 03:32:55 -0300

    Axios Retry was implemented.

--- src/helpers/scraper.js
+++ src/helpers/scraper.js
@@ -1,5 +1,18 @@
 const axios = require("axios");
+const axiosRetry = require('axios-retry');
 const extractor = require("./extractors");
+
+axiosRetry(axios, {
+	retries: 3, // number of retries
+	retryDelay: (retryCount) => {
+		// console.log(`attempt: ${retryCount}`);
+		return retryCount * 2000; // time interval between retries
+	},
+	retryCondition: (error) => {
+		// if retry condition is not specified, by default idempotent requests are retried
+		return error.response.status === 503;
+	},
+});
 
 const scraper = (url) => axios.get(url)
 	.then(res => extractor(res.data))
