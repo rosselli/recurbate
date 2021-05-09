acf47258ea0dadea18466a8d773e9e60340173a5
Flavio Rosselli <rosselli00@gmail.com>
2021-05-09 03:45:47 -0300

    Commands was refactored.

--- src/helpers/commands.js
+++ src/helpers/commands.js
@@ -1,40 +1,49 @@
 const chalk = require('chalk');
+const cyan = chalk.cyan;
+const yellow = chalk.yellow;
 
 module.exports = [
 	{
-		run: chalk.cyan('update-performers-checklist ') + chalk.yellow('<pages>'),
-		description: 'Update the performer\'s Checklists based on information requested from site.',
+		run: cyan('get-recent-recordings ') + yellow('<pages>'),
+		description: '\tGet the performers recent recordings to provide a payload for React.',
+	},
+	{
+		run: cyan('status') + yellow(' '),
+		description: '\tOutput: performer 0% watched (watched of total) 9% downloaded (downloaded of total)',
+	},
+	{
+		run: cyan('performers-update-checklist ') + yellow('<pages>'),
+		description: '\tUpdate the performer\'s Checklists based on information requested from site.',
+	},
+	{
+		run: cyan('performers-set-checked') + yellow(' '),
+		description: '\tSet performer\'s downloaded videos to checked based on hard disk file\'s list.',
+	},
+	{
+		run: cyan('create-items-checklist ') + yellow('<performer>') + ' ' + yellow('<pages>'),
+		description: '\t\tCreate the performers Checklist items. Source: recurbate.com/performer/<performer>.' +
+			'\n\t\t\t\t\t\t\tOutput: [<checked>, <videoName>, <videoLink>, <videoStatus>],\n',
+	},
+	{
+		
+		run: cyan('all-performers-set-status') + yellow(' '),
+		description: '\tSet performer\'s status in the All Performers Checklist. Source: recurbate.com/performers/.',
+	},
+	{
+
+		run: cyan('all-performers-counter') + yellow(' '),
+		description: '\tOutput: source, performers, analyzed, favorites and rejected',
 	},
 
-	{
-		run: chalk.cyan('create-items-checklist ') + chalk.yellow('<performer>') + ' ' + chalk.yellow('<pages>'),
-		description: 'Create the performers Checklist items based on information requested from site.',
-	},
 
-	{
-		run: chalk.cyan('all-performers-set-status') + chalk.yellow('       '),
-		description: 'Set performer\'s status in the All Performers Checklist based on lists got manually from site.',
-	},
-
-	{
-		run: chalk.cyan('set-videos-checked') + chalk.yellow('       '),
-		description: 'Set performer\'s downloaded videos to checked based on hard disk file\'s list.',
-	},
-
-	{
-		run: chalk.cyan('get-recent-recordings ') + chalk.yellow('<pages>'),
-		description: 'Get the performers recent recordings to provide a payload for React.',
-	},
 
 
 	// {
-	// 	run: chalk.cyan('node update-performers-checklist ' + chalk.yellow('<pages>')),
-	// 	description: '',
+	// 	run: cyan('node update-performers-checklist ' + yellow('<pages>')),
+	// 	description: '\t',
 	// },
-	//
 	// {
-	// 	run: chalk.cyan('node update-performers-checklist ' + chalk.yellow('<pages>')),
-	// 	description: '',
+	// 	run: cyan('node update-performers-checklist ' + yellow('<pages>')),
+	// 	description: '\t',
 	// },
-
 ]
