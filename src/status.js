16108289d245b72413b0a49ad44f0849e020ff5b
Flavio Rosselli <rosselli00@gmail.com>
2021-05-13 01:17:39 -0300

    Console Output was refactored.

--- src/status.js
+++ src/status.js
@@ -26,16 +26,36 @@ performers().map(performer => {
 	counter.push(total);
 });
 
-const output = []
+const output = [];
+const outputGreaterThan70 = [];
+const outputBetween70And20 = [];
+const outputLessThan20 = [];
+
 counter.map(item => {
+	const clPerformer = chalk.cyan(item.performer);
+	const clWatchedPercentage = chalk.yellow(item.watchedPercentage + '%');
+	const clDownloadedPercentage = chalk.yellow(item.downloadedPercentage + '%');
+	const clWatched = chalk.blue('watched') + chalk.green(' (' + item.watched + ' of ' + item.videos + ')')
+	const clDownloaded = chalk.blue('downloaded') + chalk.green(' (' + (item.videos - item.toDownload) + ' of ' + item.videos + ')')
+
+	if (item.watchedPercentage >= 70) {
+		outputGreaterThan70.push([clPerformer, `${clWatchedPercentage} ${clWatched}`, `${clDownloadedPercentage} ${clDownloaded}`]);
+	}
+
+	if (item.watchedPercentage > 20 && item.watchedPercentage < 70) {
+		outputBetween70And20.push([clPerformer, `${clWatchedPercentage} ${clWatched}`, `${clDownloadedPercentage} ${clDownloaded}`]);
+	}
+
+	if (item.watchedPercentage <= 20 && item.watchedPercentage > 0) {
+		outputLessThan20.push([clPerformer, `${clWatchedPercentage} ${clWatched}`, `${clDownloadedPercentage} ${clDownloaded}`]);
+	}
+
 	if (item.watchedPercentage > 0) {
-		const clPerformer = chalk.cyan(item.performer);
-		const clWatchedPercentage = chalk.yellow(item.watchedPercentage + '%');
-		const clDownloadedPercentage = chalk.yellow(item.downloadedPercentage + '%');
-		const clWatched = chalk.blue('watched') + chalk.green(' (' + item.watched + ' of ' + item.videos + ')')
-		const clDownloaded = chalk.blue('downloaded') + chalk.green(' (' + (item.videos - item.toDownload) + ' of ' + item.videos + ')')
 		output.push([clPerformer, `${clWatchedPercentage} ${clWatched}`, `${clDownloadedPercentage} ${clDownloaded}`]);
 	}
 });
 
-console.log(table(output))
+// console.log(table(output))
+console.log('Greater than 70%\n', table(outputGreaterThan70))
+console.log('Between 70% and 20%\n', table(outputBetween70And20))
+console.log('Less than 20%\n', table(outputLessThan20))
