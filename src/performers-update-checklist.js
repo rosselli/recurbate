6e1e838f92782b9266499a7be0a128184486303d
Flavio Rosselli <rosselli00@gmail.com>
2021-05-13 01:22:10 -0300

    LinksToDownload feature was implemented.

--- src/performers-update-checklist.js
+++ src/performers-update-checklist.js
@@ -3,6 +3,9 @@ const urls = require('./helpers/urls');
 const scraper = require('./helpers/scraper');
 const validateParameters = require('./helpers/cli-validations');
 const performers = require('./helpers/get-performers');
+const toDownloadPath = './data/new-videos-to-download.json';
+const toDownload = files.readJSON(toDownloadPath);
+const toDownloadPerformers = toDownload.map(item => item.performer);
 
 const run = (performer, pages) => {
 	const performerChecklist = '/Users/rosselli/systems/2021.04/local/checklist-notes-data/porn/cams/recurbate/checklists/checklist-'+ performer +'.json';
@@ -16,11 +19,25 @@ const run = (performer, pages) => {
 		let counter = 0;
 		const data = await scraper(urls.performer(performer, index + 1));
 		if (data && data.length > 0) {
-			data.map(newOne => !currentFileNames.includes(newOne.videoName) && currentPayload.items.push(itemsFormat(newOne)) && counter++);
+			data.map(newOne => {
+				if (!currentFileNames.includes(newOne.videoName)) {
+					currentPayload.items.push(itemsFormat(newOne));
+					if (!toDownloadPerformers.includes(performer)) {
+						const videoItem = { performer, videos: [newOne.videoLink] }
+						toDownloadPerformers.push(performer);
+						toDownload.push(videoItem);
+					} else {
+						const item = toDownload.find(item => item.performer == performer);
+						!item.videos.includes(newOne.videoLink) && item.videos.push(newOne.videoLink);
+					}
+					counter++;
+				}
+			});
 		}
 		if (counter > 0) {
 			console.log(performer, 'Currently', currentPayload.items.length, 'New items for ', counter);
 			sort(currentPayload.items);
+			files.writeJSON(toDownload, toDownloadPath);
 			files.writeJSON(currentPayload, performerChecklist);
 		}
 	});
@@ -31,4 +48,5 @@ if (validateParameters.pages(2, 'checklist')) {
 	// const performer = 'gabyspencer_';
 	// run(performer, pages)
 	performers().map(performer => run(performer, pages));
+
 }
